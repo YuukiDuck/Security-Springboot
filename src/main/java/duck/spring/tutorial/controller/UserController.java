28f39d0088c4f65ec1ce544dbdb11c5b0118e94e
Duc Nguyen <nqduc211223@gmail.com>
2023-12-13 13:53:16 +0700

    Initial commit

--- src/main/java/duck/spring/tutorial/controller/UserController.java
+++ src/main/java/duck/spring/tutorial/controller/UserController.java
@@ -6,7 +6,6 @@ import duck.spring.tutorial.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -38,6 +37,7 @@ public class UserController {
         return userOptional.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<User> createUser(@RequestBody User user) {
